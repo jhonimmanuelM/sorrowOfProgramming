bc6ff8fe1c0190efc4bfdc5fc7f799336d9cfdf7
Praveen Kumar H <praveenkumarh@dckap.com>
2021-02-06 17:32:47 +0530

    User page CRUD design changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,35 +1,35 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
-  <meta charset="UTF-8">
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  <title>DCKAP - Blackbox</title>
-  <!-- General CSS Files -->
-  <link rel="stylesheet" href="{{ asset('assets/css/app.min.css') }}">
-  <!-- Template CSS -->
-  <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-  <link rel="stylesheet" href="{{ asset('assets/css/components.css') }}">
-  <!-- Custom style CSS -->
-  <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
-  <link rel='shortcut icon' type='image/x-icon' href="{{ asset('assets/img/favicon.ico') }}" />
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>DCKAP - Blackbox</title>
+    <link rel='shortcut icon' type='image/x-icon' href="{{ asset('assets/img/favicon.ico') }}"/>
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="{{ asset('assets/css/app.min.css') }}">
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/components.css') }}">
+    <!-- Custom style CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/custom/custom.css') }}">
 </head>
 <body>
-    <div class="loader"></div>
-    <div id="app">
-        <div class="main-wrapper main-wrapper-1">
-            @if (Route::has('login'))
+<div class="loader"></div>
+<div id="app">
+    <div class="main-wrapper main-wrapper-1">
+        @if (Route::has('login'))
             @auth
                 @include('layouts.header')
                 @include('layouts.sidebar')
             @endif
-            @endif
-          <div class="main-content">
+        @endif
+        <div class="main-content">
             <section class="section">
                 @yield('content')
             </section>
             @include('layouts.setting-sidebar')
-          </div>
-            @include('layouts.footer')
         </div>
+        @include('layouts.footer')
     </div>
+</div>
 </body>
 </html>
