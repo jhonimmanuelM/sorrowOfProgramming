c59b121536e1fec82c84b4fc105176ae90d62749
DCKAP <alavudeenm@dckap.com>
2021-02-08 06:48:16 +0530

    Active Menu on sidebar Completed

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class DashboardController extends Controller
 {
@@ -12,6 +13,13 @@ class DashboardController extends Controller
     }
 
     public function index(){
-    	return view('dashboard.index');
+    	if(Auth::user()->hasRole(['BBA','Recruiter','Interviewer'])){
+    		return redirect()->route('nhr.all');
+    	}elseif(Auth::user()->hasRole('TL')){
+    		return redirect()->route('nhr.index');
+    	}elseif(Auth::user()->hasRole('Employee')){
+    		return redirect()->route('referrals.index');
+    	}
+    	// return view('dashboard.index');
     }
 }
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,69 +1,69 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html"> <img alt="image" src="{{ asset('assets/img/logo.png') }}" class="header-logo"/> <span
+            <a href="{{ url('/') }}"> <img alt="image" src="{{ asset('assets/img/logo.png') }}" class="header-logo"/> <span
                     class="logo-name">Otika</span>
             </a>
         </div>
         <ul class="sidebar-menu">
-            <li class="menu-header">Main</li>
+            <!-- <li class="menu-header">Main</li>
             <li class="dropdown active">
                 <a href="{{ url('/') }}" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
-            </li>
+            </li> -->
 
             @if(Auth::user()->hasRole(['BBA','Recruiter','TL']))
-                <li class="dropdown">
+                <li class="dropdown {{ Request::segment(1) === 'roles' ? 'active' : null }} {{ Request::segment(1) === 'users' ? 'active' : null }}">
                     <a href="#" class="menu-toggle nav-link has-dropdown"><i
                             data-feather="briefcase"></i><span>Employee Management</span></a>
                     <ul class="dropdown-menu">
-                        <li><a class="nav-link" href="{{ route('users.index') }}">Employees</a></li>
+                        <li class="{{ Request::segment(1) === 'users' ? 'active' : null }}"><a class="nav-link" href="{{ route('users.index') }}">Employees</a></li>
                         @if(Auth::user()->hasRole('BBA'))
-                            <li><a class="nav-link" href="{{ route('roles.index') }}">Roles</a></li>
+                            <li class="{{ Request::segment(1) === 'roles' ? 'active' : null }}"><a class="nav-link" href="{{ route('roles.index') }}">Roles</a></li>
                         @endif
                     </ul>
                 </li>
             @endif
 
-            <li class="dropdown">
+            <li class="dropdown {{ Request::segment(1) === 'referrals' ? 'active' : null }}">
                 <a href="#" class="menu-toggle nav-link has-dropdown"><i
                         data-feather="briefcase"></i><span>Referrals</span></a>
                 <ul class="dropdown-menu">
                     @if(Auth::user()->hasRole(['Recruiter','BBA']))
-                    <li><a class="nav-link" href="{{ route('referrals.all') }}">Referrals</a></li>
+                    <li class="{{ Request::segment(2) === 'all' ? 'active' : null }}"><a class="nav-link" href="{{ route('referrals.all') }}">Referrals</a></li>
                     @endif
-                    <li><a class="nav-link" href="{{ route('referrals.index') }}">My Referrals</a></li>
+                    <li class="{{ Request::segment(1) === 'referrals' && Request::segment(2) !== 'all' ? 'active' : null }}"><a class="nav-link" href="{{ route('referrals.index') }}">My Referrals</a></li>
                 </ul>
             </li>
 
             @if(Auth::user()->hasRole(['BBA','Recruiter','TL','Interviewer']))
-                <li class="dropdown">
+                <li class="dropdown {{ Request::segment(1) === 'nhr' ? 'active' : null }}">
                     <a href="#" class="menu-toggle nav-link has-dropdown"><i
                             data-feather="briefcase"></i><span>NHR</span></a>
                     <ul class="dropdown-menu">
                         @if(Auth::user()->hasRole(['BBA','Recruiter','Interviewer']))
-                            <li><a class="nav-link" href="{{ route('nhr.all') }}">NHR</a></li>
+                            <li class="{{ Request::segment(2) === 'all' ? 'active' : null }}"><a class="nav-link" href="{{ route('nhr.all') }}">NHR</a></li>
                         @endif
                         @if(Auth::user()->hasRole('TL'))
-                            <li><a class="nav-link" href="{{ route('nhr.index') }}">My NHR</a></li>
+                            <li class="{{ Request::segment(1) === 'nhr' && Request::segment(2) !== 'all' ? 'active' : null }}"><a class="nav-link" href="{{ route('nhr.index') }}">My NHR</a></li>
                         @endif
                     </ul>
                 </li>
             @endif
 
             @if(Auth::user()->hasRole('BBA'))
-                <li class="dropdown">
+                <li class="dropdown {{ Request::segment(1) === 'skills' ? 'active' : null }} {{ Request::segment(1) === 'positions' ? 'active' : null }} {{ Request::segment(1) === 'teams' ? 'active' : null }} {{ Request::segment(1) === 'checklist' ? 'active' : null }}">
                     <a href="#" class="menu-toggle nav-link has-dropdown"><i
                             data-feather="briefcase"></i><span>Setting</span></a>
                     <ul class="dropdown-menu">
-                        <li><a class="nav-link" href="{{ route('teams.index') }}">Teams</a></li>
-                        <li><a class="nav-link" href="{{ route('positions.index') }}">Positions</a></li>
-                        <li><a class="nav-link" href="{{ route('skills.index') }}">Skill Sets</a></li>
-                        <li><a class="nav-link" href="{{ route('checklist.index') }}">Employee Checklists</a></li>
+                        <li class="{{ Request::segment(1) === 'teams' ? 'active' : null }}"><a class="nav-link" href="{{ route('teams.index') }}">Teams</a></li>
+                        <li class="{{ Request::segment(1) === 'positions' ? 'active' : null }}"><a class="nav-link" href="{{ route('positions.index') }}">Positions</a></li>
+                        <li class="{{ Request::segment(1) === 'skills' ? 'active' : null }}"><a class="nav-link" href="{{ route('skills.index') }}">Skill Sets</a></li>
+                        <li class="{{ Request::segment(1) === 'checklist' ? 'active' : null }}"><a class="nav-link" href="{{ route('checklist.index') }}">Employee Checklists</a></li>
                     </ul>
                 </li>
             @endif
             @if(Auth::user()->hasRole('Recruiter'))
-                <li class="dropdown active">
+                <li class="dropdown {{ Request::segment(1) === 'candidates' ? 'active' : null }}">
                     <a href="{{ route('candidates.index') }}" class="nav-link"><i data-feather="monitor"></i><span>Candidates</span></a>
                 </li>
             @endif
